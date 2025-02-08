a927b21c89a439c34f6bc5615180640e8558ed7c
may-cook <maycook.email@gmail.com>
2025-02-08 01:35:53 +0000

    Update tag_graph_renderer to read graph from json file

--- tag_graph_renderer.py
+++ tag_graph_renderer.py
@@ -1,11 +1,18 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import json
 import pydot
 from networkx.drawing.nx_pydot import graphviz_layout
+from networkx.readwrite import json_graph
 
-def render_tag_graph(tags): #generates an image based on the graph passed to it and saves it to tagGraph.png
+def render_tag_graph(tags=None): #generates an image based on the graph passed to it and saves it to tagGraph.png
     tag_graph = nx.DiGraph()
-    tag_graph.add_edges_from(tags)
+    if (tags == None) : # if no tag data was passed, read it from file and generate a graph
+        with open('tag_graph.json', 'r') as file:
+            graph_data = json.load(file)
+            tag_graph.add_edges_from(graph_data["edges"])
+    else: # if tag data was passed, use it to generate  graph
+        tag_graph.add_edges_from(tags)
 
     fig = plt.figure("Tag History", facecolor="#5393f3")
     fig.set_figwidth(12)
@@ -23,7 +30,6 @@ def render_tag_graph(tags): #generates an image based on the graph passed to it 
 
     plt.savefig("tagGraph.png", dpi=200) # save the graph to file
 
-
 # uncomment below to call function with test data
 # tags = [("badspeed", "bergheimer"), ("badspeed", "stewart"), ("badspeed", "mae"), ("mae", "ed"), ("bergheimer", "oli"), ("bergheimer", "logan"), ("logan", "billy"), ("harry", "AJ"), ("harry", "may"), ("AJ", "jasmine"), ("jasmine", "charlotte"), ("charlotte", "joe"), ("joe", "vlad")]
-# render_tag_graph(tags)
+# render_tag_graph()
